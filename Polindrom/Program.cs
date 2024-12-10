cf699ce0a7ea3c1733d65fb2767e2511b6650a2b
Инсаф Валиуллин <valiullininsaf2@gmail.com>
2024-12-10 17:13:53 +0300

    Убрал код

--- Polindrom/Program.cs
+++ Polindrom/Program.cs
@@ -1,18 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-
-int data = 543345;
-int data2 = data;
-int b = 0;
-while (b < data)
-{
-    b = b * 10 + data2 % 10;
-    data2 = data2 / 10;
-    if (data2 == b)
-    {
-        Console.WriteLine($"{data} - полиндромное число");
-        return;
-    }
-}
-Console.WriteLine($"{data} - не полиндромное число ");
